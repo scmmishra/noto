6fbc5f1140e03ff4fccc7ffd81000a7839511f5c
Shivam Mishra <scm.mymail@gmail.com>
2022-11-17 11:18:04 +0530

    feat: update team.py

--- models/team.py
+++ models/team.py
@@ -2,14 +2,17 @@ from typing import Optional
 from pydantic import FileUrl, HttpUrl
 from sqlmodel import Field, SQLModel, Relationship
 
-from models.user import User
+# from models.user import User
 
 
 class Team(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(index=True)
+    subdomain: str = Field(index=True, unique=True, max_length=50, nullable=False)
+    owner_id: int = Field(default=None, foreign_key="user.id")
+
     website_url: Optional[HttpUrl] = Field(default=None)
+    logo: Optional[FileUrl] = Field(default=None)
     tagline: Optional[str] = Field(nullable=True)
     team_logo: Optional[FileUrl] = Field(default=None, nullable=True)
-    owner_id: int = Field(default=None, foreign_key="user.id")
     # owner: User = Relationship(back_populates="users")
