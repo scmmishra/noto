1a6368338ded3f83b9f010c94b2a42921cbd0eb4
Shivam Mishra <scm.mymail@gmail.com>
2022-11-23 20:54:40 +0530

    feat: demo script
    * reset all tables before running demo
    * add demo script for post

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,5 +1,5 @@
 import logging
-from sqlmodel import Session
+from sqlmodel import SQLModel, Session, select
 from pydantic import EmailStr, HttpUrl
 from settings import Config
 
@@ -20,12 +20,28 @@ if Config.DEBUG:
     fake = Faker()
     fake.add_provider(MarkdownPostProvider)
 
+import contextlib
+from sqlalchemy import MetaData
+
+
+def reset_db():
+    if not Config.DEBUG:
+        raise OnlyDebugError("This action is only available in debug mode.")
+    meta = MetaData()
+    print("Dropping all tables")
+    with contextlib.closing(Config.engine.connect()) as con:
+        trans = con.begin()
+        for table in reversed(SQLModel.metadata.sorted_tables):
+            print(table)
+            con.execute(table.delete())
+        trans.commit()
+
 
 def make_demo_user(first_name=None, last_name=None):
     if not Config.DEBUG:
         raise OnlyDebugError("This action is only available in debug mode.")
 
-    first_name = first_name if first_name else fake.first_name()
+    first_name = first_name if first_name else fake.unique.first_name()
 
     return User(
         first_name=first_name,
@@ -45,6 +61,7 @@ def make_demo_team(owner: User, name=None, tagline=None, website_url=None):
         name=name if name else fake.company(),
         tagline=tagline if tagline else fake.catch_phrase(),
         website_url=HttpUrl(url=url, scheme="https"),
+        team_logo=fake.file_path(depth=2, category="image"),
         subdomain=name.lower() if name else fake.slug(),
         owner_id=owner.id or 0,
         owner=owner,
@@ -55,10 +72,10 @@ def make_demo_post(team: Team):
     if not Config.DEBUG:
         raise OnlyDebugError("This action is only available in debug mode.")
 
-    post = ChangelogPost(
+    return ChangelogPost(
         title=fake.sentence(),
         short_description=fake.paragraph(nb_sentences=3),
-        content=fake.markdown_post(),
+        content=fake.post(size="medium"),
         for_team_id=team.id,
         published_on=fake.date_time_between(start_date="-1y", end_date="now"),
         author_id=team.owner_id,
@@ -70,18 +87,23 @@ def build():
     if not Config.DEBUG:
         raise OnlyDebugError("This action is only available in debug mode.")
 
-    faris = make_demo_user("Faris", "Ansari")
-    shivam = make_demo_user("Shivam", "Mishra")
-
-    users = [faris, shivam]
-
-    for _ in range(10):
-        user = make_demo_user()
-        users.append(user)
+    reset_db()
 
     with Session(Config.engine) as session:
+        faris = make_demo_user("Faris", "Ansari")
+        shivam = make_demo_user("Shivam", "Mishra")
+
+        users = [faris, shivam]
+
+        for _ in range(10):
+            user = make_demo_user()
+            users.append(user)
+
         session.add_all(users)
         session.commit()
+
+        session.refresh(faris)
+        session.refresh(shivam)
 
         gameplan = make_demo_team(
             faris,
@@ -92,12 +114,21 @@ def build():
 
         session.add(gameplan)
         session.commit()
+        session.refresh(gameplan)
+
         gameplan.add_member(faris, RoleEnum.admin)
         gameplan.add_member(shivam, RoleEnum.admin)
 
         for user in users:
+            if user.id in [faris.id, shivam.id]:
+                continue
+            session.refresh(user)
             gameplan.add_member(user)
 
+        posts = []
         for _ in range(25):
             post = make_demo_post(gameplan)
-            session.add(post)
+            posts.append(post)
+
+        session.add_all(posts)
+        session.commit()
