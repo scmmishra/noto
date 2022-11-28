930c138fa9e42a098e30a762656c09c50c335b94
Shivam Mishra <scm.mymail@gmail.com>
2022-11-28 20:35:36 +0530

    feat: update changelog post models

--- api/public/post/models.py
+++ api/public/post/models.py
@@ -11,17 +11,13 @@ from api.public.team.models import Team
 from api.public.user.models import User
 
 
-class ChangelogPost(TimeStampMixin, SQLModel, table=True):
-    """ChangelogPost model"""
-
-    id: int = Field(default=None, primary_key=True)
+class ChangelogPostBase(TimeStampMixin, SQLModel):
     title: str = Field(max_length=120, nullable=False)
     slug: str = Field(max_length=120, nullable=False, unique=True)
     short_description: str = Field(max_length=250, nullable=False)
     content: str = Field(sa_column=Column(TEXT))
     hero_image: Optional[FileUrl] = Field(default=None)
     for_team_id: int = Field(default=None, foreign_key="team.id")
-    for_team: Team = Relationship(back_populates="changelog_posts")
     tags: Optional[str] = Field(default=None, nullable=True)
 
     published_on: Optional[datetime] = Field(
@@ -31,5 +27,25 @@ class ChangelogPost(TimeStampMixin, SQLModel, table=True):
             nullable=True,
         )
     )
+
+
+class ChangelogPost(ChangelogPostBase, table=True):
+    """ChangelogPost model"""
+
+    id: int = Field(default=None, primary_key=True)
     author_id: Optional[int] = Field(default=None, foreign_key="user.id")
+    for_team: Team = Relationship(back_populates="changelog_posts")
     author: "User" = Relationship(back_populates="changelog_posts")
+
+
+class ChangelogPostCreate(ChangelogPostBase):
+    pass
+
+
+class CHangelogPostRead(ChangelogPostBase):
+    id: int
+    author_id: int
+
+
+class CHangelogPostUpdate(ChangelogPostBase):
+    pass
